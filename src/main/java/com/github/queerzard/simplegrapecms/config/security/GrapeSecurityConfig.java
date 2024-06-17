e4deffe7666daf94d3e23ce40eb8862a9aeb068c
queerzard <ozan0.aslan5@gmail.com>
2024-06-18 01:07:50 +0200

    edited GrapeSecurityConfig

--- src/main/java/com/github/queerzard/simplegrapecms/config/security/GrapeSecurityConfig.java
+++ src/main/java/com/github/queerzard/simplegrapecms/config/security/GrapeSecurityConfig.java
@@ -1,14 +1,19 @@
 package com.github.queerzard.simplegrapecms.config.security;
 
+import com.github.queerzard.simplegrapecms.database.services.auth.GrapeUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Order(value = -1)
 @Configuration
@@ -16,9 +21,58 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @EnableMethodSecurity(securedEnabled = true, jsr250Enabled = true)
 public class GrapeSecurityConfig {
 
-    public SecurityFilterChain filterChain(){
-        return null;
+    private final GrapeAuthenticationProvider authenticationProvider;
+
+    @Autowired
+    private GrapeUserDetailsService userDetailsService;
+
+    @Autowired
+    public GrapeSecurityConfig(GrapeAuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
     }
+
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((registry) -> {
+                    registry
+                            .requestMatchers(HttpMethod.DELETE).hasRole("ROLE_ADMIN")
+                            .requestMatchers(
+                                    "/",
+                                    "/home",
+                                    "/login"
+                            ).permitAll()
+
+                            .requestMatchers(
+                                    "/logout"
+                            ).authenticated()
+
+                            .anyRequest()
+                            .authenticated()
+                    ;
+                })
+
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(sessionConfigurer -> {
+                    sessionConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+                });
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return this.userDetailsService;
+    }
+/*
+
+    @Bean
+    public AuthenticationManager authenticationManager(){
+        return new AuthenticationManagerBuilder().authenticationProvider(this.authenticationProvider).build();
+    }
+*/
 
 
 }
